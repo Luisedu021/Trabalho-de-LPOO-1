776cd22b1482afb503bb4600bf043a68643de24b
guiescb <guilhermeescobarassis@gmail.com>
2026-05-18 19:22:44 -0300

    Update Class.java

--- lpoo/stable/Class.java
+++ lpoo/stable/Class.java
@@ -18,6 +18,24 @@ public class Class
   //fazendo uma nova arv para guardar os atributos da classe(variaveis da variable.java)
   private TreeMap<String, Variable> attributes;
 
+  //Fazendo uma nova arv para suportar construtores
+  private TreeMap<String, Constructor> constructors;
+
+  //construtor padrão (sem superclasse)
+  public Class(String name){
+    this(name, null);
+  }
+
+  //construtor completo (com superclasse opcional)
+  public Class(String name, Class superClass){
+    this.name = name;
+    this.superClass = superClass;
+    this.methods = new TreeMap<>();
+    //inicializando para evitar NullPointerException
+    this.attributes = new Treemap<>();
+    this.constructors = new TreeMap<>();
+  }
+  
     //metodo para adicionar um metodo na árvore da classe
 
     public void addMethod(Method novoMetodo) throws DuplicateSymbolException{
@@ -29,10 +47,16 @@ public class Class
 
     public Method findMethod(String name){
         return this.methods.get(name);
+      
+        //Se não achar aqui, estende a busca para a superclasse (busca ascendente)
+        if (metodo == null && this.superClass != null){
+          return this.superClass.findMethod(name);
+        }
+        return metodo;
     }
 
   //metodo p add atributo na classe
-
+  
   public void addAttribute(Variable novaVariavel) throws DuplicateSymbolException{
         if(this.attributes.containsKey(novaVariavel.getName())){
             throw new DuplicateSymbolException();
@@ -44,13 +68,38 @@ public class Class
 
   public Variable findAttribute(String name){
         return this.attributes.get(name);
+
+        // Se não achar aqui, estende a busca para a superclasse (busca ascendente)
+        if (atributo == null && this.superClass != null){
+          return this.superClass.findAttribute(name);
+        }
+        return atributo;
   }
 
-    //Construtor
-    public Class(String name) {
-        this.name = name;
-        this.methods = new TreeMap<>();
-    }
-  public String getName() {return this.name ; }
+  //métodos para construtores(novo)
 
+  public void addConstructor(Constructor novoConstrutor) throws DuplicateSymbolException{
+        if (this.constructors.containsKey(novoConstrutor.getName())){
+        throw new DuplicateSymbolException();
+        }
+        this.constructors.put(novoConstrutor.getName(), novoConstrutor);
+  }
+
+  public Constructor findConstructor(String name){
+    //Construtores não são herdados pela superclasse, então a busca é apenas local
+    return this.constructors.get(name);
+  }
+
+  //getters e setters
+  public String getName(){
+    return this.name;
+  }
+
+  public Class getSuperclass(){
+    return this.superClass;
+  }
+
+  public void setSuperClass(Class superClass){
+    this.superClass = superClass;
+  }
 };
