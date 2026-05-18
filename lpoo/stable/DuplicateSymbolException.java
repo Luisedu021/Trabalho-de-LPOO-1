4274a37775c2074947aa829015a25b13e972ae9a
guiescb <guilhermeescobarassis@gmail.com>
2026-05-18 20:18:30 -0300

    Update DuplicateSymbolException.java

--- lpoo/stable/DuplicateSymbolException.java
+++ lpoo/stable/DuplicateSymbolException.java
@@ -1,11 +1,22 @@
 package lpoo.stable;
 
 /**
- *
- * @author // put your name(s) here
+ * @author Luís Eduardo Lopes dos Santos
+ * @author Otávio Ferreira Augusto
+ * @author Guilherme Escobar
  */
 public class DuplicateSymbolException extends Exception
 {
-  // insert your code here
-  
+  // id de serialização padrão para classes que estendem Exception
+  private static final long serialVersionUID = 1L;
+
+  //construtor padrão
+  public DuplicateSymbolException() {
+    super("Símbolo duplicado detectado no escopo atual.");
+  }
+
+  // construtor personalizado que aceita o noome do símbolo que causou o erro
+  public DuplicateSymbolException(String symbolName) {
+    super("Erro: O símbolo '" + symbolName + "' ja foi declarado neste escopo.");
+  }
 } // DuplicateSymbolException
